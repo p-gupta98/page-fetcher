7442ce8fee3dfa1f618e77e18008002a2ef1a831
vagrant <pihugupta0501@gmail.com>
2024-01-20 22:51:07 +0000

    Added request and fs libraries using require. Step 2: Saved the command line arguments in variables. Step 3: Wrote a basic function to make an http request. Step 4: Wrote a basic function to write the body of the url to a file.

---
+++ fetcher.js
@@ -0,0 +1,42 @@
+const request = require('request');
+
+const fs = require("fs");
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// console.log('BEFORE writeFile call');
+
+// fs.writeFile("./test_async.txt", "h3ll0 file!\n", (error) => {
+//   if (error) {
+//     // Handle error
+//     console.log("Failed to write to file");
+//     return;
+//   }
+//   // Success!
+//   console.log("Successfully wrote to file");
+// });
+
+// console.log('AFTER writeFile call');
+
+
+// const request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('error', error);
+  } else {
+    fs.writeFile('path', body, (errorTwo) => {
+      if(errorTwo) {
+        console.log('Failed to write to File');
+      }
+      console.log('File written succesfully');
+    });
+  }
+} );
