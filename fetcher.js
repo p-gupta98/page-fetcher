5d45f8815783c333422abc06030a978adf0dd08a
vagrant <pihugupta0501@gmail.com>
2024-01-20 23:32:10 +0000

    function running successfully

--- fetcher.js
+++ fetcher.js
@@ -5,31 +5,6 @@ const fs = require("fs");
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// console.log('BEFORE writeFile call');
-
-// fs.writeFile("./test_async.txt", "h3ll0 file!\n", (error) => {
-//   if (error) {
-//     // Handle error
-//     console.log("Failed to write to file");
-//     return;
-//   }
-//   // Success!
-//   console.log("Successfully wrote to file");
-// });
-
-// console.log('AFTER writeFile call');
-
-
-// const request = require('request');
-// request('http://www.google.com', function (error, response, body) {
-//   console.error('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-
-//Downloaded and saved 1235 bytes to ./index.html.
-
 const fetcher = function(url, filePath) {
 
   request(url, function (error, response, body) {
